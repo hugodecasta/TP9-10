c619f3b1e163cdd75bb83a99cdef2b9b7ec180b6
CASTANEDA HUGO p1608557 <hugodecasta@orange.fr>
2016-11-30 15:09:37 +0100

    src base

--- src/graphe.cpp
+++ src/graphe.cpp
@@ -1,3 +1,26 @@
 #include "graphe.hpp"
 
 //////////////////////////////// METHODS
+
+
+//----
+void initGrid(Grid& g, unsigned width, unsigned height)
+{
+}
+//----
+void pruneGrid(Grid& g)
+{
+}
+//----
+void setVertex(Grid& g, unsigned i, unsigned j, vertex v)
+{
+}
+//----
+vertex getVertex(const Grid& g, unsigned i, unsigned j, orient o)
+{
+}
+//----
+void drawGrid(const Grid& g)
+{
+}
+
