ad6dd681ca43cad03ab5af69e01410b81322103e
CASTANEDA HUGO p1608557 <hugodecasta@orange.fr>
2016-11-30 16:40:16 +0100

    exemple file

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 #include "graphe.hpp"
+#include <stdlib.h>
+
+/*
+void fillGraph(Graph& g)
+{
+	cout << g.width << " " << g.height << endl;
+	for(unsigned i = 0; i < g.height; ++i)
+		for(unsigned j = 0; j < g.width; ++j)
+		{
+			vertex v = rand()%100;
+			cout << j << " " << i << " " << v << endl;
+		}
+}
+*/
 
 int main()
 {
-	string filename = "exemple.grille";
+	string filename = "exemple.grd";
 	Graph g;
 
 	loadGraph(filename,g);
